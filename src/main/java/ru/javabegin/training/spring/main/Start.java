30a4f34a03788e5c9c0244b7d00e4fe08840156c
Eugene Zonov <eugene.zonov@gmail.com>
2017-11-23 14:25:29 +0700

    New robot model T3000

--- src/main/java/ru/javabegin/training/spring/main/Start.java
+++ src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ru.javabegin.training.spring.impls.robot.ModelT1000;
 import ru.javabegin.training.spring.impls.robot.ModelT2000;
+import ru.javabegin.training.spring.impls.robot.ModelT3000;
 
 public class Start {
 	public static void main(String[] args) {
@@ -19,5 +20,9 @@ public class Start {
 		ModelT2000 t2000 = (ModelT2000) context.getBean("t2000");
 		t2000.dance();
 		t2000.action();
+		
+		ModelT3000 t3000 = (ModelT3000) context.getBean("t3000");
+		t3000.dance();
+		t3000.action();
 	}
 }
